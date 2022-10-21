992d08538c34ef1f8484346dd0d959d832161f42
eight <eight@eightnoiMac.local>
2022-10-22 03:34:31 +0900

    [update]: syndromeを作成

--- src/main.rs
+++ src/main.rs
@@ -63,18 +63,28 @@ impl FiniteField{
 		}
 		FiniteField{char:self.char,length:self.length,elements:result}
 	}
+	fn toVec(&self)->Vec<u8>{
+		let mut result:Vec<u8> = Vec::new();
+		for i in 0..self.elements.len(){
+			result.push(self.elements[i as usize].num as u8);
+		}
+		result
+	}
 	
 }
 
+// u係数のx変数多項式
 fn function(x:&PrimeField, u:&FiniteField, char:&u8, length:&u8) ->PrimeField{
 	let mut result = PrimeField{char:*char,num:0};
 	for i in 0..*length{
-		result = result.add(&x.pow(i.into()).mul(&u.elements[i as usize]));
+		let mut tmp = &x.pow(i.into()).mul(&u.elements[i as usize]);
+		result = result.add(&tmp);
+
 	}
 	result
 	
 }
-fn encode(P:Vec<PrimeField>,origin_sentense:FiniteField, char:&u8, length:&u8)->FiniteField{
+fn encode(P:&Vec<PrimeField>,origin_sentense:FiniteField, char:&u8, length:&u8)->FiniteField{
 	let mut u = Vec::new();
 	for i in 0..char-1{
 		let mut temp = function(&P[i as usize], &origin_sentense, char, length);
@@ -84,8 +94,18 @@ fn encode(P:Vec<PrimeField>,origin_sentense:FiniteField, char:&u8, length:&u8)->
 
 }
 fn main() {
-	let char = 11;
-	let length = 5;
+	let char = 11; // n+1
+	let length =5;
+
+	let n = &char-1; // 符号込の長さ
+	let k = &length; // 文章の長さ
+	let d = n - k + 1; // 最小距離
+	let t = (n-k)/2;
+	
+	let l_0 = &n - 1 - &t;
+	let l_1 = &n - 1 - &t - (k - 1);
+	println!("n:{},k:{},d:{},t:{},l_0:{},l_1:{}",n,k,d,t,l_0,l_1);
+
 	// 送りたい文章
 	let origin_sentense = FiniteField::new(char,length,
 										   vec![PrimeField::new(char,0),
@@ -94,21 +114,34 @@ fn main() {
 												PrimeField::new(char,0),
 												PrimeField::new(char,0)]);
 
+
 	let mut P = Vec::new();
 	// 原始根を作成
+	let primitive_element:i32 = 2;
+	
 	for i in 0..char-1{
-		P.push(PrimeField::new(char,2_i32.pow(i.into())));
+		P.push(PrimeField::new(char,primitive_element.pow(i.into())));
 	}
 	P = FiniteField::new(char,length,P).elements;
 	// 符号化
-	let u = encode(P,origin_sentense,&char,&length);
+	let u = encode(&P,origin_sentense,&char,&length);
 
-	let mut u_send = Vec::new();
-	for i in 0..char-1{
-		u_send.push(u.elements[i as usize].num);
+	// 送信でエラーを起こす
+	//let mut u_received = u.toVec();
+	let mut u_received = vec![5,9,0,9,0,1,0,7,0,5];
+	
+
+	let u_received = FiniteField::new(char, length, u_received.into_iter().map(|x|PrimeField::new(char,x as i32)).collect());
+	
+	let mut S:Vec<PrimeField> = Vec::new();
+	for i in 1..n-k{
+		S.push(function(&P[i as usize], &u_received, &char, &n));
 	}
+	println!("S:{:?}",S);
+	
 
-	println!("{:?}",u_send);
+	
+
 
 
 }
